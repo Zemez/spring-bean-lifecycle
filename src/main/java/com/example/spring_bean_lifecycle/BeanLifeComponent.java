e7748e9b6a7e4b70f6a4448e8d8d53d50886c16d
Andrey Inozemtsev <a.zemez@yandex.ru>
2019-11-30 16:47:46 +0300

    add InitializingBean

--- src/main/java/com/example/spring_bean_lifecycle/BeanLifeComponent.java
+++ src/main/java/com/example/spring_bean_lifecycle/BeanLifeComponent.java
@@ -1,9 +1,13 @@
 package com.example.spring_bean_lifecycle;
 
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -12,9 +16,10 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@EqualsAndHashCode
+@Data
 @Component
-public class BeanLifeComponent implements DisposableBean {
+//@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.DEFAULT)
+public class BeanLifeComponent implements InitializingBean, DisposableBean {
 
     private static final Logger logger = LoggerFactory.getLogger(BeanLifeComponent.class);
     private int phase = 0;
@@ -26,6 +31,11 @@ public class BeanLifeComponent implements DisposableBean {
     @PostConstruct
     public void postConstruct() {
         logPhase("post construct");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logPhase("after properties set");
     }
 
     @EventListener(ContextRefreshedEvent.class)
